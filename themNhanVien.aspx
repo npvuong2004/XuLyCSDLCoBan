f842c114c7f6714fb665445f4698434a89b7c3a9
npvuong2004 <148404004+npvuong2004@users.noreply.github.com>
2024-03-19 10:12:34 +0700

    Add files via upload

---
+++ themNhanVien.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="themNhanVien.aspx.cs" Inherits="QLNhanVien.themNhanVien" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div>
+        Nhập họ nhân viên: <asp:TextBox ID="txtHo" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        Nhập tên nhân viên: <asp:TextBox ID="txtTen" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        Chọn Giói tinh: <asp:RadioButton ID="rdNam" runat="server" Text="Nam" GroupName="GT"/>
+                        <asp:RadioButton ID="rdNu" runat="server" Text="Nữ" GroupName="GT"/>
+    </div>
+    <div>
+        Nhập ngày sinh: <asp:TextBox ID="txtNgaySinh" runat="server" TextMode="Date"></asp:TextBox>
+    </div>
+    <div>
+        Nhập nơi sinh: <asp:TextBox ID="txtNoiSinh" runat="server"></asp:TextBox>
+    </div>
+    <div>
+        Nhập phòng: <asp:DropDownList ID="ddlPhong" runat="server" DataSourceID="dsPhong" DataTextField="TenPhong" DataValueField="MaPhong"></asp:DropDownList>
+    </div>
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Lưu" />
+        <asp:Label ID="lbThongBao" runat="server" Text="" ForeColor="Red"></asp:Label>
+    </div>
+
+    <asp:SqlDataSource ID="dsPhong" runat="server"
+         ConnectionString="<%$ ConnectionStrings:QLNhanVienConnectionString %>" SelectCommand="SELECT * FROM [PhongBan]"></asp:SqlDataSource>
+        
+    
+</asp:Content>
---
+++ themNhanVien.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QLNhanVien
+{
+    public partial class themNhanVien : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        protected void btnLuu_Click1(object sender, EventArgs e)
+        {
+            string chuoi_ket_noi = ConfigurationManager.ConnectionStrings["QLNhanVienConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(chuoi_ket_noi);
+            conn.Open();
+            string sql = "insert into nhanvien(honv,tennv,phai,ngaysinh,noisinh,maphong) values (@honv,@tennv,@phai,@ngaysinh,@noisinh,@maphong)";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@honv", txtHo.Text);
+            cmd.Parameters.AddWithValue("@tennv", txtTen.Text);
+            if (rdNam.Checked)
+                cmd.Parameters.AddWithValue("@phai", true);
+            else
+                cmd.Parameters.AddWithValue("@phai", false);
+
+            cmd.Parameters.AddWithValue("@ngaysinh", txtNgaySinh.Text);
+            cmd.Parameters.AddWithValue("@noisinh", txtNoiSinh.Text);
+            cmd.Parameters.AddWithValue("@maphong", ddlPhong.SelectedValue);
+            if (cmd.ExecuteNonQuery() > 0)
+                Response.Redirect("qlNhanVien.aspx");
+            else
+                lbThongBao.Text = "Thao tác thêm nhân viên thất bại";
+        }
+    }
+}
